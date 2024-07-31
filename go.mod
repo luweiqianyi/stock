c1382050597c313002f74d05facf8460d33a06f6
luweiqianyi <runningriven@163.com>
2024-07-31 10:53:42 +0800

    开发环境准备

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module stock
+
+go 1.22.1
+
+require github.com/zeromicro/go-zero v1.7.0 // indirect
---
+++ scripts/prepare_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# go version need greater than 1.16
+go install github.com/zeromicro/go-zero/tools/goctl@latest
